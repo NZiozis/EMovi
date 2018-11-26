989b315943385e382ef51200573dd1657751724f
nikoziozis <nikoziozis@gmail.com>
2018-11-26 13:46:48 -0500

    Initialized all of the models. Made a basic controller for the movies and made it so that all of the movies will print out onto a blank page as a test to make sure that the communication occurs

---
+++ EMovi/Controllers/MovieController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EMovi.Models;
+
+namespace EMovi.Controllers
+{
+    public class MovieController : Controller
+    {
+        DatabaseEntities _db = new DatabaseEntities();
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var model = _db.Movies.ToList();
+
+            return View(model);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
