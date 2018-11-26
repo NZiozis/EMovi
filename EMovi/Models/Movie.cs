3cf5b782739c139f3a7f86377b3d322db57b1b9d
nikoziozis <nikoziozis@gmail.com>
2018-11-25 23:42:54 -0500

    Added Models for Rates and Movie tables and created a basic DbSet in order to query them from the database.

---
+++ EMovi/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMovi.Models
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime RelaseDate { get; set; }
+        public ICollection<Rates> Ratings { get; set; }
+    }
+}
---
+++ EMovi/Models/Rates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EMovi.Models
+{
+    public class Rates
+    {
+        public int UserId { get; set; }
+        public int MovieId { get; set; }
+        public int Rating { get; set; }
+    }
+}
